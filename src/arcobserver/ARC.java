16086e9dfc48d8e30956321c3b4605a0b2cfbc3e
v8269590 <v8269590@linux39.scm.tees.ac.uk>
2018-10-23 14:56:22 +0100

    Created basic template

---
+++ src/arcobserver/ARC.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arcobserver;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author v8269590
+ */
+public class ARC {
+    
+    ArrayList<Subscriber> Subscribers;
+
+    public ARC() 
+    {
+        this.Subscribers = new ArrayList<>();
+    }
+    
+    public void subscribe(Subscriber s)
+    {
+        Subscribers.add(s);
+    }
+    
+    public void unsubscribe(Subscriber s)
+    {
+        Subscribers.remove(s);
+    }
+    
+    public void notifyOfPlane(Plane p)
+    {
+        for(Subscriber s : Subscribers)
+        {
+            s.update(p);
+        }
+    }
+}
---
+++ src/arcobserver/ARCObserver.java
@@ -0,0 +1,12 @@
+
+package arcobserver;
+
+
+public class ARCObserver {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
